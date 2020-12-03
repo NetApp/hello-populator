3f2420d59111b28df4dffa598924058be52e31df
Ben Swartzlander <ben@swartzlander.org>
2020-12-03 11:19:31 -0500

    Refactor the client code into a dedicated directory
    Also simplify the PV rebind code.

--- controller.go
+++ controller.go
@@ -18,6 +18,7 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
@@ -42,11 +43,11 @@ import (
 	coreinformers "k8s.io/client-go/informers/core/v1"
 	corelisters "k8s.io/client-go/listers/core/v1"
 
-	hellov1 "github.com/kubernetes-csi/hello-populator/pkg/apis/hello/v1alpha1"
-	helloclientset "github.com/kubernetes-csi/hello-populator/pkg/client/clientset/versioned"
-	hellofactory "github.com/kubernetes-csi/hello-populator/pkg/client/informers/externalversions"
-	helloinformers "github.com/kubernetes-csi/hello-populator/pkg/client/informers/externalversions/hello/v1alpha1"
-	hellolisters "github.com/kubernetes-csi/hello-populator/pkg/client/listers/hello/v1alpha1"
+	hellov1 "github.com/kubernetes-csi/hello-populator/client/apis/hello/v1alpha1"
+	helloclientset "github.com/kubernetes-csi/hello-populator/client/clientset/versioned"
+	hellofactory "github.com/kubernetes-csi/hello-populator/client/informers/externalversions"
+	helloinformers "github.com/kubernetes-csi/hello-populator/client/informers/externalversions/hello/v1alpha1"
+	hellolisters "github.com/kubernetes-csi/hello-populator/client/listers/hello/v1alpha1"
 )
 
 const (
@@ -70,7 +71,7 @@ func main() {
 		showVersion  bool
 	)
 	// Main arg
-	flag.StringVar(&mode, "mode", "", "Mode to run in (controller, backup, restore)")
+	flag.StringVar(&mode, "mode", "", "Mode to run in (controller, populate)")
 	// Populate args
 	flag.StringVar(&fileName, "file-name", "", "File name to populate")
 	flag.StringVar(&fileContents, "file-contents", "", "Contents to populate file with")
@@ -145,7 +146,7 @@ func runController(masterURL, kubeconfig, imageName string) {
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
 	helloInformerFactory := hellofactory.NewSharedInformerFactory(helloClient, time.Second*30)
 
-	controller := newController(kubeClient, helloClient, imageName,
+	controller := newController(kubeClient, imageName,
 		kubeInformerFactory.Core().V1().PersistentVolumeClaims(),
 		kubeInformerFactory.Core().V1().PersistentVolumes(),
 		kubeInformerFactory.Core().V1().Pods(),
@@ -166,21 +167,20 @@ type stringSet struct {
 }
 
 type controller struct {
-	kubeclientset  kubernetes.Interface
-	helloclientset helloclientset.Interface
-	imageName      string
-	pvcLister      corelisters.PersistentVolumeClaimLister
-	pvcSynced      cache.InformerSynced
-	pvLister       corelisters.PersistentVolumeLister
-	pvSynced       cache.InformerSynced
-	podLister      corelisters.PodLister
-	podSynced      cache.InformerSynced
-	helloLister    hellolisters.HelloLister
-	helloSynced    cache.InformerSynced
-	mu             sync.Mutex
-	notifyMap      map[string]*stringSet
-	cleanupMap     map[string]*stringSet
-	workqueue      workqueue.RateLimitingInterface
+	kubeclientset kubernetes.Interface
+	imageName     string
+	pvcLister     corelisters.PersistentVolumeClaimLister
+	pvcSynced     cache.InformerSynced
+	pvLister      corelisters.PersistentVolumeLister
+	pvSynced      cache.InformerSynced
+	podLister     corelisters.PodLister
+	podSynced     cache.InformerSynced
+	helloLister   hellolisters.HelloLister
+	helloSynced   cache.InformerSynced
+	mu            sync.Mutex
+	notifyMap     map[string]*stringSet
+	cleanupMap    map[string]*stringSet
+	workqueue     workqueue.RateLimitingInterface
 }
 
 func (c *controller) callMeLater(keyToCall, objType, namespace, name string) {
@@ -227,7 +227,6 @@ func (c *controller) cleanupNofications(keyToCall string) {
 
 func newController(
 	kubeclientset kubernetes.Interface,
-	helloclientset helloclientset.Interface,
 	imageName string,
 	pvcInformer coreinformers.PersistentVolumeClaimInformer,
 	pvInformer coreinformers.PersistentVolumeInformer,
@@ -236,20 +235,19 @@ func newController(
 ) *controller {
 
 	controller := &controller{
-		kubeclientset:  kubeclientset,
-		helloclientset: helloclientset,
-		imageName:      imageName,
-		pvcLister:      pvcInformer.Lister(),
-		pvcSynced:      pvcInformer.Informer().HasSynced,
-		pvLister:       pvInformer.Lister(),
-		pvSynced:       pvInformer.Informer().HasSynced,
-		podLister:      podInformer.Lister(),
-		podSynced:      podInformer.Informer().HasSynced,
-		helloLister:    helloInformer.Lister(),
-		helloSynced:    helloInformer.Informer().HasSynced,
-		notifyMap:      make(map[string]*stringSet),
-		cleanupMap:     make(map[string]*stringSet),
-		workqueue:      workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
+		kubeclientset: kubeclientset,
+		imageName:     imageName,
+		pvcLister:     pvcInformer.Lister(),
+		pvcSynced:     pvcInformer.Informer().HasSynced,
+		pvLister:      pvInformer.Lister(),
+		pvSynced:      pvInformer.Informer().HasSynced,
+		podLister:     podInformer.Lister(),
+		podSynced:     podInformer.Informer().HasSynced,
+		helloLister:   helloInformer.Lister(),
+		helloSynced:   helloInformer.Informer().HasSynced,
+		notifyMap:     make(map[string]*stringSet),
+		cleanupMap:    make(map[string]*stringSet),
+		workqueue:     workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
 	}
 
 	pvcInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
@@ -402,7 +400,7 @@ func (c *controller) runWorker() {
 				utilruntime.HandleError(fmt.Errorf("invalid resource key: %s", key))
 				return nil
 			}
-			err = c.syncPvc(key, parts[1], parts[2])
+			err = c.syncPvc(context.TODO(), key, parts[1], parts[2])
 		default:
 			utilruntime.HandleError(fmt.Errorf("invalid resource key: %s", key))
 			return nil
@@ -427,7 +425,7 @@ func (c *controller) runWorker() {
 	}
 }
 
-func (c *controller) syncPvc(key, namespace, pvcName string) error {
+func (c *controller) syncPvc(ctx context.Context, key, namespace, pvcName string) error {
 	if workingNamespace == namespace {
 		return nil
 	}
@@ -484,25 +482,16 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 		return err
 	}
 
-	// Look for PV
-	c.callMeLater(key, "pv", "", populatePvName(pvc))
-	pv, err := c.pvLister.Get(populatePvName(pvc))
-	if nil != err {
-		if !errors.IsNotFound(err) {
-			return err
-		}
-	}
-
 	// *** Here is the first place we start to create/modify objects ***
 
-	// If the PVC is unbound, we need to perform the restore
+	// If the PVC is unbound, we need to perform the population
 	if "" == pvc.Spec.VolumeName {
 
 		// Ensure the PVC has a finalizer on it so we can clean up the stuff we create
 		if finalizers, needUpdate := ensureFinalizer(pvc, pvcFinalizer, true); needUpdate {
 			pvc = pvc.DeepCopy()
 			pvc.Finalizers = finalizers
-			_, err = c.kubeclientset.CoreV1().PersistentVolumeClaims(namespace).Update(pvc)
+			_, err = c.kubeclientset.CoreV1().PersistentVolumeClaims(namespace).Update(ctx, pvc, metav1.UpdateOptions{})
 			if nil != err {
 				return err
 			}
@@ -510,14 +499,14 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 
 		// If the pod doesn't exist yet, create it
 		if 0 == len(podList) {
-			_, err = c.kubeclientset.CoreV1().Pods(workingNamespace).Create(makePopulatePod(labelValue, pvc, c.imageName, hello))
+			_, err = c.kubeclientset.CoreV1().Pods(workingNamespace).Create(ctx, makePopulatePod(labelValue, pvc, c.imageName, hello), metav1.CreateOptions{})
 			if nil != err {
 				return err
 			}
 
 			// If PVC' doesn't exist yet, create it
 			if 0 == len(pvcList) {
-				_, err = c.kubeclientset.CoreV1().PersistentVolumeClaims(workingNamespace).Create(makeRestorePvc(labelValue, pvc))
+				_, err = c.kubeclientset.CoreV1().PersistentVolumeClaims(workingNamespace).Create(ctx, makeRestorePvc(labelValue, pvc), metav1.CreateOptions{})
 				if nil != err {
 					return err
 				}
@@ -531,7 +520,7 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 		if corev1.PodSucceeded != pod.Status.Phase {
 			if corev1.PodFailed == pod.Status.Phase {
 				// Delete failed pods so we can try again
-				err = c.kubeclientset.CoreV1().Pods(workingNamespace).Delete(pod.Name, nil)
+				err = c.kubeclientset.CoreV1().Pods(workingNamespace).Delete(ctx, pod.Name, metav1.DeleteOptions{})
 				if nil != err {
 					return err
 				}
@@ -542,14 +531,14 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 
 		// This would be bad
 		if 0 == len(pvcList) {
-			return fmt.Errorf("Failed to find PVC for retore pod")
+			return fmt.Errorf("Failed to find PVC for populator pod")
 		}
 		pvcPrime := pvcList[0]
 
-		// Get PV'
-		var pvPrime *corev1.PersistentVolume
+		// Get PV
+		var pv *corev1.PersistentVolume
 		c.callMeLater(key, "pv", "", pvcPrime.Spec.VolumeName)
-		pvPrime, err = c.kubeclientset.CoreV1().PersistentVolumes().Get(pvcPrime.Spec.VolumeName, metav1.GetOptions{})
+		pv, err = c.kubeclientset.CoreV1().PersistentVolumes().Get(ctx, pvcPrime.Spec.VolumeName, metav1.GetOptions{})
 		if nil != err {
 			if errors.IsNotFound(err) {
 				// We'll get called again later when the PV exists
@@ -558,19 +547,19 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 			return err
 		}
 
-		// Clone PV' to PV
-		if nil == pv {
-			pv := &corev1.PersistentVolume{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:        populatePvName(pvc),
-					Annotations: pvPrime.Annotations,
-				},
-			}
+		claimRef := pv.Spec.ClaimRef
+		if claimRef.Name != pvcName || claimRef.Namespace != pvc.Namespace ||
+			claimRef.UID != pvc.UID {
+			// Modify PV
+			pv = pv.DeepCopy()
 			if nil == pv.Annotations {
 				pv.Annotations = make(map[string]string)
 			}
 			pv.Annotations[populatedFromAnno] = hello.Namespace + "/" + hello.Name
-			pvPrime.Spec.DeepCopyInto(&pv.Spec)
+			if nil == pv.Labels {
+				pv.Labels = make(map[string]string)
+			}
+			pv.Labels[targetLabel] = labelValue
 			pv.Spec.ClaimRef = &corev1.ObjectReference{
 				APIVersion:      "v1",
 				Kind:            "PersistentVolumeClaim",
@@ -579,43 +568,22 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 				UID:             pvc.UID,
 				ResourceVersion: pvc.ResourceVersion,
 			}
-			_, err = c.kubeclientset.CoreV1().PersistentVolumes().Create(pv)
+			_, err = c.kubeclientset.CoreV1().PersistentVolumes().Update(ctx, pv, metav1.UpdateOptions{})
 			if nil != err {
 				return err
 			}
-		}
 
-		// Update PV'
-		if pvPrime.Spec.PersistentVolumeReclaimPolicy != corev1.PersistentVolumeReclaimRetain {
-			pvPrime = pvPrime.DeepCopy()
-			if nil == pvPrime.Labels {
-				pvPrime.Labels = make(map[string]string)
-			}
-			pvPrime.Labels[targetLabel] = labelValue
-			pvPrime.Spec.PersistentVolumeReclaimPolicy = corev1.PersistentVolumeReclaimRetain
-			_, err = c.kubeclientset.CoreV1().PersistentVolumes().Update(pvPrime)
-			if nil != err {
-				return err
-			}
+			// Don't start cleaning up yet -- we need to bind controller to acknowledge
+			// the switch
+			return nil
 		}
+	}
 
-		// Don't start cleaning up yet -- we need to bind controller to acknowledge
-		// the switch
-		return nil
-	} else {
-		// Look for PV'
-		var pvList []*corev1.PersistentVolume
-		pvList, err = c.pvLister.List(labels.SelectorFromSet(selectorSet))
-		if nil != err {
-			return err
-		}
-
-		if 0 < len(pvList) {
-			pvPrime := pvList[0]
-			err = c.kubeclientset.CoreV1().PersistentVolumes().Delete(pvPrime.Name, nil)
-			if nil != err {
-				return err
-			}
+	// Wait for the bind controller to rebind the PV
+	if 0 < len(pvcList) {
+		pvcPrime := pvcList[0]
+		if corev1.ClaimLost != pvcPrime.Status.Phase {
+			return nil
 		}
 	}
 
@@ -624,7 +592,7 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 	// If the pod still exists, delete it
 	if 0 < len(podList) {
 		pod := podList[0]
-		err = c.kubeclientset.CoreV1().Pods(workingNamespace).Delete(pod.Name, nil)
+		err = c.kubeclientset.CoreV1().Pods(workingNamespace).Delete(ctx, pod.Name, metav1.DeleteOptions{})
 		if nil != err {
 			return err
 		}
@@ -633,7 +601,7 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 	// If PVC' still exists, delete it
 	if 0 < len(pvcList) {
 		pvcPrime := pvcList[0]
-		err = c.kubeclientset.CoreV1().PersistentVolumeClaims(workingNamespace).Delete(pvcPrime.Name, nil)
+		err = c.kubeclientset.CoreV1().PersistentVolumeClaims(workingNamespace).Delete(ctx, pvcPrime.Name, metav1.DeleteOptions{})
 		if nil != err {
 			return err
 		}
@@ -643,7 +611,7 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 	if finalizers, needUpdate := ensureFinalizer(pvc, pvcFinalizer, false); needUpdate {
 		pvc = pvc.DeepCopy()
 		pvc.Finalizers = finalizers
-		_, err = c.kubeclientset.CoreV1().PersistentVolumeClaims(namespace).Update(pvc)
+		_, err = c.kubeclientset.CoreV1().PersistentVolumeClaims(namespace).Update(ctx, pvc, metav1.UpdateOptions{})
 		if nil != err {
 			return err
 		}
@@ -656,10 +624,6 @@ func (c *controller) syncPvc(key, namespace, pvcName string) error {
 
 func populatePvcName(targetPvc *corev1.PersistentVolumeClaim) string {
 	return fmt.Sprintf("prime-%s", targetPvc.UID)
-}
-
-func populatePvName(targetPvc *corev1.PersistentVolumeClaim) string {
-	return fmt.Sprintf("pvc-%s", targetPvc.UID)
 }
 
 func makeRestorePvc(labelValue string, targetPvc *corev1.PersistentVolumeClaim) *corev1.PersistentVolumeClaim {
--- go.mod
+++ go.mod
@@ -1,13 +1,11 @@
 module github.com/kubernetes-csi/hello-populator
 
-go 1.13
+go 1.15
 
 require (
-	github.com/aws/aws-sdk-go v1.28.5
-	github.com/google/martian v2.1.0+incompatible
-	k8s.io/api v0.17.1
-	k8s.io/apimachinery v0.17.1
-	k8s.io/client-go v0.17.1
-	k8s.io/code-generator v0.17.1
-	k8s.io/sample-controller v0.17.1
+	k8s.io/api v0.18.10
+	k8s.io/apimachinery v0.18.10
+	k8s.io/client-go v0.18.10
+	k8s.io/code-generator v0.18.10
+	k8s.io/sample-controller v0.18.10 // indirect
 )
